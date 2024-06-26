5306f3424ff0438e03c720170905f8fd15fd1628
luizpsg <luiz.saud@sga.pucminas.br>
2024-06-26 10:29:00 -0300

    chore: Refactor create_book endpoint to use BookRequest model

--- 4-Project-2-books/books.py
+++ 4-Project-2-books/books.py
@@ -1,4 +1,5 @@
 from fastapi import Body, FastAPI
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
@@ -10,6 +11,14 @@ class Book:
         self.author = author
         self.description = description
         self.rating = rating
+
+
+class BookRequest(BaseModel):
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
 
 
 BOOKS = [
@@ -28,6 +37,7 @@ async def read_all_books():
 
 
 @app.post("/create_book")
-async def create_book(book_request=Body()):
-    BOOKS.append(book_request)
-    return book_request
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.model_dump())
+    BOOKS.append(new_book)
+    return new_book
