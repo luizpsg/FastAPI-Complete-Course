85d8230eb5b57a576b6b81d8fd8a8bdce4ddb8bc
luizpsg <luiz.saud@sga.pucminas.br>
2024-06-26 13:44:56 -0300

    chore: Add API endpoint to read a specific todo by ID

--- 5-Project-3-todos/main.py
+++ 5-Project-3-todos/main.py
@@ -1,9 +1,10 @@
 from typing import Annotated
 from sqlalchemy.orm import Session
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, Path
 import models
 from models import Todos
 from database import engine, SessionLocal
+from starlette import status
 
 app = FastAPI()
 
@@ -21,6 +22,14 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
-@app.get("/")
+@app.get("/todos", status_code=status.HTTP_200_OK)
 async def read_all(db: db_dependency):
     return db.query(Todos).all()
+
+
+@app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+async def read_one(db: db_dependency, todo_id: int = Path(gt=0)):
+    todo = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo is not None:
+        return todo
+    raise HTTPException(status_code=404, detail="Todo not found")
