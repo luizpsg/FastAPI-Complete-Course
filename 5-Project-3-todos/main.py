fede2e8829d034dc37e56a60822edeb62249f15e
luizpsg <luiz.saud@sga.pucminas.br>
2024-06-26 14:05:07 -0300

    chore: Update todo with PUT request endpoint

--- 5-Project-3-todos/main.py
+++ 5-Project-3-todos/main.py
@@ -52,3 +52,22 @@ async def create_todo(db: db_dependency, todo_request: TodoRequest):
     db.refresh(new_todo)
 
     return new_todo
+
+
+@app.put("/todos/{todo_id}", status_code=status.HTTP_200_OK)
+async def update_todo(
+    db: db_dependency, todo_request: TodoRequest, todo_id: int = Path(gt=0)
+):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is None:
+        raise HTTPException(status_code=404, detail="Todo not found")
+
+    todo_model.title = todo_request.title
+    todo_model.description = todo_request.description
+    todo_model.priority = todo_request.priority
+    todo_model.completed = todo_request.completed
+
+    db.commit()
+    db.refresh(todo_model)
+
+    return todo_model
