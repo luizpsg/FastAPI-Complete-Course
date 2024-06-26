b0f246b9670062887c666606a18c7e0f0c3bb291
luizpsg <luiz.saud@sga.pucminas.br>
2024-06-26 17:06:03 -0300

    chore: Add authentication requirement for creating a new todo

--- 5-Project-3-todos/routers/todos.py
+++ 5-Project-3-todos/routers/todos.py
@@ -5,6 +5,7 @@ from fastapi import APIRouter, Depends, HTTPException, Path
 from models import Todos
 from database import SessionLocal
 from starlette import status
+from .auth import get_current_user
 
 router = APIRouter(tags=["todos"])
 
@@ -18,6 +19,7 @@ def get_db():
 
 
 db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
 
 
 class TodoRequest(BaseModel):
@@ -41,8 +43,14 @@ async def read_one(db: db_dependency, todo_id: int = Path(gt=0)):
 
 
 @router.post("/todos", status_code=status.HTTP_201_CREATED)
-async def create_todo(db: db_dependency, todo_request: TodoRequest):
-    new_todo = Todos(**todo_request.model_dump())
+async def create_todo(
+    user: user_dependency, db: db_dependency, todo_request: TodoRequest
+):
+
+    if user is None:
+        raise HTTPException(status_code=401, detail="Authentication required")
+
+    new_todo = Todos(**todo_request.model_dump(), owner_id=user.get("id"))
 
     db.add(new_todo)
     db.commit()
