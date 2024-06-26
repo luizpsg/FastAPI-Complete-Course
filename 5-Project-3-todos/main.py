f6b2eeef75cc578906972b234cb958d662e52ae2
luizpsg <luiz.saud@sga.pucminas.br>
2024-06-26 13:54:12 -0300

    chore: Add API endpoint to create a new todo

--- 5-Project-3-todos/main.py
+++ 5-Project-3-todos/main.py
@@ -1,4 +1,5 @@
 from typing import Annotated
+from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
 from fastapi import FastAPI, Depends, HTTPException, Path
 import models
@@ -22,14 +23,32 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
+class TodoRequest(BaseModel):
+    title: str = Field(min_length=3, max_length=50)
+    description: str = Field(min_length=5, max_length=100)
+    priority: int = Field(ge=1, le=5)
+    completed: bool
+
+
 @app.get("/todos", status_code=status.HTTP_200_OK)
 async def read_all(db: db_dependency):
     return db.query(Todos).all()
 
 
-@app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+@app.get("/todos/{todo_id}", status_code=status.HTTP_200_OK)
 async def read_one(db: db_dependency, todo_id: int = Path(gt=0)):
     todo = db.query(Todos).filter(Todos.id == todo_id).first()
     if todo is not None:
         return todo
     raise HTTPException(status_code=404, detail="Todo not found")
+
+
+@app.post("/todos", status_code=status.HTTP_201_CREATED)
+async def create_todo(db: db_dependency, todo_request: TodoRequest):
+    new_todo = Todos(**todo_request.model_dump())
+
+    db.add(new_todo)
+    db.commit()
+    db.refresh(new_todo)
+
+    return new_todo
