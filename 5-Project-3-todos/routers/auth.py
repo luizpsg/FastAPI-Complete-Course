d4ef70aeed87a4ef749522ffc589bcb15d1d8184
luizpsg <luiz.saud@sga.pucminas.br>
2024-06-26 16:50:47 -0300

    chore: Update auth.py to include OAuth2PasswordBearer for token authentication

--- 5-Project-3-todos/routers/auth.py
+++ 5-Project-3-todos/routers/auth.py
@@ -1,14 +1,14 @@
 from datetime import datetime, timedelta, timezone
 from typing import Annotated
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
 from models import Users
 from passlib.context import CryptContext
 from database import SessionLocal
 from sqlalchemy.orm import Session
 from starlette import status
-from fastapi.security import OAuth2PasswordRequestForm
-from jose import jwt
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from jose import jwt, JWTError
 
 router = APIRouter()
 
@@ -16,6 +16,7 @@ SECRET_KEY = "53B9BCF5732AFCC8E4CCEC168A28B8233EB21061DA0A7B969EBBF69F480DD879"
 ALGORITHM = "HS256"
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="token")
 
 
 def get_db():
@@ -43,6 +44,24 @@ def create_access_token(username: str, user_id: int, expires_delta: timedelta):
     expires = datetime.now(timezone.utc) + expires_delta
     enconde.update({"exp": expires})
     return jwt.encode(enconde, SECRET_KEY, algorithm=ALGORITHM)
+
+
+def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        user_id: int = payload.get("id")
+        if username is None or user_id is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Could not validate credentials",
+            )
+        return {"username": username, "id": user_id}
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+        )
 
 
 class CreateUserRequest(BaseModel):
