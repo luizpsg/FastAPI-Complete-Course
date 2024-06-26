f8eedd9e73d6f8c0453eef7018a3e3552e7cda7b
luizpsg <luiz.saud@sga.pucminas.br>
2024-06-26 17:17:11 -0300

    chore: Update update_todo endpoint to include authentication and owner validation

--- 5-Project-3-todos/routers/todos.py
+++ 5-Project-3-todos/routers/todos.py
@@ -72,9 +72,21 @@ async def create_todo(
 
 @router.put("/todos/{todo_id}", status_code=status.HTTP_200_OK)
 async def update_todo(
-    db: db_dependency, todo_request: TodoRequest, todo_id: int = Path(gt=0)
+    user: user_dependency,
+    db: db_dependency,
+    todo_request: TodoRequest,
+    todo_id: int = Path(gt=0),
 ):
-    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if user is None:
+        raise HTTPException(status_code=401, detail="Authentication required")
+
+    todo_model = (
+        db.query(Todos)
+        .filter(Todos.id == todo_id)
+        .filter(Todos.owner_id == user.get("id"))
+        .first()
+    )
+
     if todo_model is None:
         raise HTTPException(status_code=404, detail="Todo not found")
 
