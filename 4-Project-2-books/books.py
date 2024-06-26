1be76a08ef0f09ed74455a04bb49dca10bc62be9
luizpsg <luiz.saud@sga.pucminas.br>
2024-06-26 11:23:31 -0300

    chore: Refactor create_book endpoint to use BookRequest model

--- 4-Project-2-books/books.py
+++ 4-Project-2-books/books.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import Body, FastAPI
 from pydantic import BaseModel, Field
 
@@ -14,11 +15,16 @@ class Book:
 
 
 class BookRequest(BaseModel):
-    id: int
-    title: str
-    author: str
-    description: str
-    rating: int
+    id: Optional[int] = (
+        Field(
+            title="The ID of the book", description="This is an auto-generated field"
+        ),
+        None,
+    )
+    title: str = Field(min_length=3, max_length=50)
+    author: str = Field(min_length=3, max_length=50)
+    description: str = Field(min_length=5, max_length=100)
+    rating: int = Field(ge=1, le=5)
 
 
 BOOKS = [
@@ -39,5 +45,10 @@ async def read_all_books():
 @app.post("/create_book")
 async def create_book(book_request: BookRequest):
     new_book = Book(**book_request.model_dump())
-    BOOKS.append(new_book)
+    BOOKS.append(find_book_id(new_book))
     return new_book
+
+
+def find_book_id(book: Book):
+    book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
+    return book
