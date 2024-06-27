5dd3a985dbe379d286b3cc2b509debe8058c2bb3
luizpsg <luiz.saud@sga.pucminas.br>
2024-06-26 22:11:12 -0300

    chore: Add phone_number column to Users table

---
+++ 5-Project-3-todos/alembic/versions/223cda81a1c7_create_phone_number_for_user_column.py
@@ -0,0 +1,27 @@
+"""create phone number for user column
+
+Revision ID: 223cda81a1c7
+Revises: 
+Create Date: 2024-06-26 22:04:45.960821
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "223cda81a1c7"
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.add_column("users", sa.Column("phone_number", sa.String, nullable=True))
+
+
+def downgrade() -> None:
+    pass
